0b11b83b63e508301946219b906cca163d6d52d6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-31 10:35:45 +0500

    CreateWikipediaUrl method implemented in WikiService.

--- WikiInsight/Service/WikiService.cs
+++ WikiInsight/Service/WikiService.cs
@@ -1,12 +1,37 @@
+using System.Net;
+
 namespace WikiInsight.Service;
 
 public class WikiService
 {
     private readonly HttpClient client;
-    private readonly string API_BASE_URL = "https://en.wikipedia.org/w/api.php");
+    private static readonly string API_BASE_URL = "https://en.wikipedia.org/w/api.php";
 
     public WikiService(IHttpClientFactory factory)
     {
         client = factory.CreateClient("wikiClient");
+    }
+
+    static string CreateWikipediaUrl(string title, bool full)
+    {
+        var urlBuilder = new UriBuilder(API_BASE_URL);
+        var queryString = new Dictionary<string, string>
+        {
+            ["action"] = "query",
+            ["prop"] = "extracts",
+            ["format"] = "json",
+            ["formatversion"] = "2",
+            ["redirects"] = "1",
+            ["explaintext"] = "1",
+            ["exsectionformat"] = "wiki",
+            ["titles"] = title
+        };
+
+        if (!full)
+            queryString["exintro"] = "1";
+
+        urlBuilder.Query = string.Join("&", queryString.Select(kv => $"{WebUtility.UrlEncode(kv.Key)}={WebUtility.UrlEncode(kv.Value)}"));
+
+        return urlBuilder.ToString();
     }
 }
