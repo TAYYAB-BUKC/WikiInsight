73a557f242899128b26bc1efc0baf32f136638ea
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-04-10 23:17:24 +0500

    FindTopKArticleChunks method implemented in VectorSearchService.

--- WikiInsight/Service/VectorSearchService.cs
+++ WikiInsight/Service/VectorSearchService.cs
@@ -1,8 +1,9 @@
 using Pinecone;
+using WikiInsight.Models;
 
 namespace WikiInsight.Service;
 
-public class VectorSearchService(EmbeddingService embeddingService, IndexClient pineconeIndexClient, DocumentStoreService contentStore)
+public class VectorSearchService(EmbeddingService embeddingService, IndexClient pineconeIndexClient, DocumentStoreService contentStore, ArticleChunkStoreService articleChunkStoreService)
 {
     public async Task<List<Article>> FindTopKArticles(string query, int k)
     {
@@ -24,6 +25,36 @@ public class VectorSearchService(EmbeddingService embeddingService, IndexClient 
 
         var ids = matches.Select(m => m.Id!).Where(id => !string.IsNullOrEmpty(id));
         var articles = contentStore.GetArticles(ids);
+
+        var scoreById = matches.Where(m => m.Id is not null).ToDictionary(m => m.Id!, m => m.Score);
+
+        var orderedArticles = articles.OrderByDescending(a => scoreById.GetValueOrDefault(a.Id, 0f)).Take(k).ToList();
+
+        return orderedArticles;
+    }
+
+    public async Task<List<ArticleChunk>> FindTopKArticleChunks(string query, int k)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+            return [];
+
+        var embeddings = await embeddingService.GenerateEmbeddings(query);
+
+        var response = await pineconeIndexClient.QueryAsync(new QueryRequest()
+        {
+            TopK = 3,
+            Vector = embeddings,
+            IncludeMetadata = true
+        });
+
+        var matches = (response.Matches ?? []).ToList();
+
+        if (matches.Count == 0)
+            return [];
+
+        var ids = matches.Select(m => m.Id!).Where(id => !string.IsNullOrEmpty(id));
+
+        var articles = articleChunkStoreService.GetArticleChunks(ids);
 
         var scoreById = matches.Where(m => m.Id is not null).ToDictionary(m => m.Id!, m => m.Score);
 
